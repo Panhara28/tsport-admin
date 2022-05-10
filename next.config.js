9290bfabb5dc03e4969103999c8a593361c534a3
Reachea <reacheasambath@gmail.com>
2022-05-10 16:13:13 +0700

    Stopped tracking .env File

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['s1.moc.gov.kh', 's2.moc.gov.kh', 'cpp-s1.cpp-production.moc.gov.kh'],
+    domains: ['s1.moc.gov.kh', 's2.moc.gov.kh', 'cpp-s1.cpp-production.moc.gov.kh', 'cpp-s1.moc.gov.kh'],
   },
 };
 
