e53d32ed1bf9adc2b993060c23f61254ce377796
aipanhara <titpanhara@gmail.com>
2022-05-16 13:19:46 +0700

    fix

--- libs/client.ts
+++ libs/client.ts
@@ -22,3 +22,8 @@ export default function createApolloClient(token = '') {
     },
   });
 }
+
+export const myClient = new ApolloClient({
+  uri: process.env.NEXT_PUBLIC_API_URL,
+  cache: new InMemoryCache(),
+});
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,20 +7,26 @@ import { TokenContainer } from '../src/components/Authentication/TokenContext';
 import ApolloContext from '../src/components/Authentication/ApolloContext';
 import Authentication from '../src/components/Authentication/Authentication';
 import AppScreen from '../src/components/AppScreen';
+import { ApolloProvider } from '@apollo/client';
+import { useMemo } from 'react';
+import createApolloClient, { myClient } from '../libs/client';
 
 function MyApp({ Component, pageProps }: any) {
   return (
-    <Provider store={store}>
-      <TokenContainer>
-        <ApolloContext>
-          <Authentication>
-            <AppScreen>
-              <Component {...pageProps} />
-            </AppScreen>
-          </Authentication>
-        </ApolloContext>
-      </TokenContainer>
-    </Provider>
+    // <Provider store={store}>
+    //   <TokenContainer>
+    //     <ApolloContext>
+    //       <Authentication>
+    //         <AppScreen>
+    //           <Component {...pageProps} />
+    //         </AppScreen>
+    //       </Authentication>
+    //     </ApolloContext>
+    //   </TokenContainer>
+    // </Provider>
+    <ApolloProvider client={myClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
   );
 }
 
