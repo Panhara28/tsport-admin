001874334f8d11ff4d7544c478902c6c1ff91346
mocdevs <it.support@moc.gov.kh>
2022-05-29 17:54:42 +0700

    fix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['s1.moc.gov.kh', 's2.moc.gov.kh', 'cpp-s1.cpp-production.moc.gov.kh', 'cpp-s1.moc.gov.kh'],
+    domains: ['imagerium.blockerium.com'],
   },
 };
 
