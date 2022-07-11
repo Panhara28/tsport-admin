1a3cb9ff774d62627551a008365e5b24eded424a
Reachea <reacheasambath@gmail.com>
2022-07-11 11:04:08 +0700

    fix label year for import filter gdce statistic

--- src/Screens/websites/Cms/Statistics/GDCE/Overview/functions/format_imports_exports_bar.ts
+++ src/Screens/websites/Cms/Statistics/GDCE/Overview/functions/format_imports_exports_bar.ts
@@ -79,6 +79,8 @@ export function format_imports_exports_bar(importsEachYear?: any, exportsEachYea
     };
 
     for (const y of importsEachYear) {
+      data.options.xaxis.categories.push(y.year);
+
       import_data.data.push(y.value);
     }
 
