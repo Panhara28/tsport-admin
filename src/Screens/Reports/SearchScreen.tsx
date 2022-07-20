0de2df631779689dbc1082e0eecbba837c1fec8a
Reachea <reacheasambath@gmail.com>
2022-07-21 06:36:11 +0700

    add filter

--- src/Screens/Reports/SearchScreen.tsx
+++ src/Screens/Reports/SearchScreen.tsx
@@ -1,4 +1,3 @@
-import Select from 'react-select';
 import { Col } from 'reactstrap';
 import { Row } from 'reactstrap';
 import { CardBody } from 'reactstrap';
@@ -11,6 +10,8 @@ import { setting } from '../../libs/settings';
 import { gql } from 'apollo-boost';
 import { useQuery } from '@apollo/client';
 import Link from 'next/link';
+import XForm from '../../components/Form/XForm';
+import { useState } from 'react';
 
 const QUERY = gql`
   query employeeReport($pagination: PaginationInput, $filter: EmployeeReportFilter) {
@@ -26,12 +27,123 @@ const QUERY = gql`
   }
 `;
 
-function RenderReport() {
+const HR_DEPARTMENT = gql`
+  query hrDepartmentList($branch_level: Int, $parent_id: Int) {
+    hrDepartmentList(branch_level: $branch_level, parent_id: $parent_id)
+  }
+`;
+
+const GeneralDepartmentSelect = ({ generalDepartmentId, setGeneralDepartmentId }: any) => {
+  const { data, loading } = useQuery(HR_DEPARTMENT, {
+    variables: {
+      branch_level: 0,
+    },
+  });
+
+  if (!data || loading) return <></>;
+
+  return (
+    <XForm.Select
+      label="Select General Department"
+      value={generalDepartmentId}
+      items={[
+        {
+          text: 'Choose one of the following',
+          value: undefined,
+        },
+        ...data?.hrDepartmentList?.map((x: any) => {
+          return {
+            text: x?.name,
+            value: x?.id,
+          };
+        }),
+      ]}
+      onChange={(e: any) => setGeneralDepartmentId(e.target.value)}
+    />
+  );
+};
+
+const DepartmentSelect = ({ generalDepartmentId, departmentId, setDepartmentId }: any) => {
+  const { data, loading } = useQuery(HR_DEPARTMENT, {
+    variables: {
+      branch_level: 1,
+      parent_id: Number(generalDepartmentId),
+    },
+  });
+
+  if (!data || loading) return <></>;
+
+  return (
+    <XForm.Select
+      label="Select Department"
+      value={departmentId}
+      onChange={(e: any) => setDepartmentId(e?.target?.value)}
+      items={[
+        {
+          text: 'Choose one of the following',
+          value: undefined,
+        },
+        ...data?.hrDepartmentList?.map((x: any) => {
+          return {
+            text: x?.name,
+            value: x?.id,
+          };
+        }),
+      ]}
+    />
+  );
+};
+
+const OfficeSelect = ({ departmentId, officeId, setOfficeId }: any) => {
+  const { data, loading } = useQuery(HR_DEPARTMENT, {
+    variables: {
+      branch_level: 2,
+      parent_id: Number(departmentId),
+    },
+  });
+
+  if (!data || loading) return <></>;
+
+  return (
+    <XForm.Select
+      label="Select Office"
+      value={officeId}
+      onChange={(e: any) => setOfficeId(e.target.value)}
+      items={[
+        {
+          text: 'Choose one of the following',
+          value: undefined,
+        },
+        ...data?.hrDepartmentList?.map((x: any) => {
+          return {
+            text: x?.name,
+            value: x?.id,
+          };
+        }),
+      ]}
+    />
+  );
+};
+
+type RenderReportProps = {
+  generalDepartmentId?: number;
+  departmentId?: number;
+  officeId?: number;
+  officerName?: string;
+};
+
+function RenderReport({ generalDepartmentId, departmentId, officeId, officerName }: RenderReportProps) {
   const { data, loading } = useQuery(QUERY, {
     variables: {
       pagination: {
         page: 1,
         size: 10,
+      },
+      filter: {
+        generalDepartmentId,
+        departmentId,
+        officeId,
+        officerName,
       },
     },
   });
@@ -61,6 +173,11 @@ function RenderReport() {
 }
 
 export function SearchScreen() {
+  const [departmentId, setDepartmentId] = useState('');
+  const [generalDepartmentId, setGeneralDepartmentId] = useState('');
+  const [officeId, setOfficeId] = useState('');
+  const [officerName, setOfficerName] = useState('');
+
   return (
     <Layout>
       <div className="page-content">
@@ -73,16 +190,28 @@ export function SearchScreen() {
                 <CardBody>
                   <Row>
                     <Col md={3}>
-                      <Select />
+                      <GeneralDepartmentSelect
+                        generalDepartmentId={generalDepartmentId}
+                        setGeneralDepartmentId={setGeneralDepartmentId}
+                      />
                     </Col>
                     <Col md={3}>
-                      <Select />
+                      <DepartmentSelect
+                        departmentId={departmentId}
+                        setDepartmentId={setDepartmentId}
+                        generalDepartmentId={generalDepartmentId}
+                      />
                     </Col>
                     <Col md={3}>
-                      <Select />
+                      <OfficeSelect officeId={officeId} setOfficeId={setOfficeId} departmentId={departmentId} />
                     </Col>
                     <Col md={3}>
-                      <input className="form-control" />
+                      <label>Search</label>
+                      <input
+                        className="form-control"
+                        value={officerName}
+                        onChange={e => setOfficerName(e.target.value)}
+                      />
                     </Col>
                   </Row>
                 </CardBody>
@@ -105,7 +234,12 @@ export function SearchScreen() {
                       </tr>
                     </thead>
                     <tbody>
-                      <RenderReport />
+                      <RenderReport
+                        generalDepartmentId={Number(generalDepartmentId)}
+                        departmentId={Number(departmentId)}
+                        officeId={Number(officeId)}
+                        officerName={officerName}
+                      />
                     </tbody>
                   </Table>
                 </CardBody>
