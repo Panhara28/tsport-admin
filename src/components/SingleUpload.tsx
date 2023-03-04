b9789fc5e52cc74927de266306447cd96c621b86
Sokphal Adam <sokphaladam@gmail.com>
2023-03-05 00:21:13 +0700

    fix

--- src/components/SingleUpload.tsx
+++ src/components/SingleUpload.tsx
@@ -7,6 +7,8 @@ import React, { useState } from 'react';
 import toastr from 'toastr';
 import 'toastr/build/toastr.min.css';
 import { parseImageUrl } from '../hook/parseImageUrl';
+import { UploadFileToFirebase } from '../libs/firebase';
+import { getDownloadURL } from 'firebase/storage';
 
 export const UPLOAD = gql`
   mutation singleUpload($file: Upload!) {
@@ -42,46 +44,61 @@ export function SingleUpload({ setImage, image, width, height }: Props) {
     },
   });
 
+  const onUpload = async (file: File) => {
+    const upload = await UploadFileToFirebase(file);
+
+    const downloadURL = await getDownloadURL(upload.ref);
+
+    console.log(downloadURL);
+
+    await setPic(downloadURL);
+    await setImage(downloadURL);
+  };
+
   const onChange = (e: any) => {
     if (e.target.validity.valid && e.target.files) {
-      singleUpload({
-        variables: {
-          file: e.target.files[0],
-        },
-      });
+      // singleUpload({
+      //   variables: {
+      //     file: e.target.files[0],
+      //   },
+      // });
+      onUpload(e.target.files[0]);
     }
   };
 
-  if (image) {
-    return (
-      <div>
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'end',
-            backgroundColor: 'unset',
-            flexDirection: 'row',
-            position: 'absolute',
-            top: 25,
-            right: 20,
-            zIndex: 999,
-          }}
-          onClick={() => {
-            setImage(undefined);
-          }}
-        >
-          <FontAwesomeIcon icon={faTrash} style={{ cursor: 'pointer' }} className="text-danger mb-3" />
-        </div>
-        <div>
-          {image ? (
-            <img src={image} alt="" style={{ width: width ? width : 280, height: height ? height : 280 }} />
-          ) : (
-            undefined
-          )}
-        </div>
-      </div>
-    );
-  }
+  // if (image) {
+  //   return (
+  //     <div>
+  //       <div
+  //         style={{
+  //           display: 'flex',
+  //           justifyContent: 'end',
+  //           backgroundColor: 'unset',
+  //           flexDirection: 'row',
+  //           position: 'absolute',
+  //           top: 25,
+  //           right: 20,
+  //           zIndex: 999,
+  //         }}
+  //         onClick={() => {
+  //           setImage(undefined);
+  //         }}
+  //       >
+  //         <FontAwesomeIcon icon={faTrash} style={{ cursor: 'pointer' }} className="text-danger mb-3" />
+  //       </div>
+  //       <div>
+  //         {image ? (
+  //           <img
+  //             src={image}
+  //             alt=""
+  //             style={{ width: width ? width : 280, height: height ? height : 280 }} />
+  //         ) : (
+  //           undefined
+  //         )}
+  //       </div>
+  //     </div>
+  //   );
+  // }
 
   return (
     <>
@@ -97,11 +114,17 @@ export function SingleUpload({ setImage, image, width, height }: Props) {
           justifyContent: 'center',
           alignItems: 'center',
           cursor: 'pointer',
+          overflow: 'hidden',
+          position: 'relative',
         }}
         onClick={() => refUpload?.current?.click()}
       >
         <input type="file" onChange={onChange} style={{ display: 'none' }} ref={refUpload} />
-        <FontAwesomeIcon icon={faImage} style={{ fontSize: 32, color: '#4886ff' }} />
+        {image ? (
+          <img src={image} alt="" style={{ width, height: 'auto', objectFit: 'contain' }} />
+        ) : (
+          <FontAwesomeIcon icon={faImage} style={{ fontSize: 32, color: '#4886ff' }} />
+        )}
       </div>
     </>
   );
